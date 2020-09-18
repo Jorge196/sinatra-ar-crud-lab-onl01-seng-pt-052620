2e39ed95628efb12b66d9b1506729aac979f0d65
Jorge Ramirez <joramirez96@gmail.com>
2020-09-18 22:56:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,38 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  
+  get '/articles/new' do
+      @article = Article.new
+      erb :new
+    end
+
+    post '/articles' do
+      @article = Article.create(title: params[:title], content: params[:content])
+      redirect to "/articles/#{@article.id}"
+    end
+
+    get '/articles/:id' do
+      @article = Article.find(params[:id])
+      erb :show
+    end
+
+    get '/articles/:id/edit' do
+      @article = Article.find(params[:id])
+      erb :edit
+    end
+
+
+    patch "/articles/:id" do
+      @article = Article.find(params[:id])
+      @article.update(params[:article])
+      redirect to "/articles/#{ @article.id }"
+    end
+
+
+    delete '/articles/:id' do
+      Article.delete(params[:id])
+      redirect to "/articles"
+    end
 
 
 end
